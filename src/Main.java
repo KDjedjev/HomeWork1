a711054198507efa8a7db21470346e757df10531
kokodzh <kdjedjev@gmail.com>
2016-05-08 12:26:14 +0300

    Homework done.

---
+++ src/Main.java
@@ -0,0 +1,102 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//zadacha 1
+			/*	Scanner num = new Scanner(System.in);
+				System.out.print("a = ");
+				float a = num.nextFloat();
+				System.out.print("b = ");
+				float b = num.nextFloat();
+				System.out.println("Liceto e: ");
+				System.out.print(a*b);
+		
+		//zadacha 2
+				int c = 5;
+				int d = 8;
+				int e;
+				e=d;
+				d=c;
+				c=e;
+				System.out.println(c);
+				System.out.println(d);
+
+		//zadacha 3
+				Scanner name = new Scanner(System.in);
+				System.out.print("First name: ");
+				String firstName = name.nextLine();
+				System.out.print("Last Name: ");
+				String lastName = name.nextLine();
+				System.out.print("Age: ");
+				short years = name.nextShort();
+				System.out.print("Gender: ");
+				char Gender = name.next().charAt(0);
+				System.out.print("ID: ");
+				long id = name.nextLong();
+				System.out.print("Number: ");
+				long number = name.nextLong();
+				
+		//zadacha 4
+				String name = "I will read the \"Harry Potter\" book collection";
+				System.out.print(name);
+		
+		//zadacha 5	
+				Scanner sum = new Scanner(System.in);
+				System.out.print("a: ");
+				int a = sum.nextInt();
+				System.out.print("b: ");
+				int b = sum.nextInt();
+				System.out.print("c: ");
+				int c = sum.nextInt();
+				System.out.print("d: ");
+				int d = sum.nextInt();
+				System.out.print(a+b+c+d);			
+	
+		//zadacha 6
+				Scanner abc = new Scanner(System.in);
+				System.out.print("r= ");
+				short r = abc.nextShort();
+				System.out.print("P=");
+				System.out.println(2*Math.PI*r);
+				System.out.print("S=");
+				System.out.print(Math.PI*r*r);
+		
+		//zadacha 7
+				Scanner dig = new Scanner(System.in);
+				System.out.print("a= ");
+				int a = dig.nextInt();
+				boolean var = (a%5==0 && a%7==0);
+				System.out.print(var);
+				
+		//zadacha 8
+				Scanner dig = new Scanner(System.in);
+				System.out.print("a=");
+				int a = dig.nextInt();
+				if(a%2==0){
+					System.out.print("Chetno");
+				}
+				else{
+					System.out.print("Nechetno");
+				}  
+				
+		//zadacha 9
+	
+				Scanner dig = new Scanner(System.in);
+				System.out.print("a=");
+				int a = dig.nextInt();
+				System.out.print("b=");
+				int b = dig.nextInt();
+				System.out.print("Ostatuk:" +(a%2) +" i " +(b%2));  
+	
+		//zadacha 10
+				Scanner tr = new Scanner(System.in);
+				System.out.print("a=");
+				int a = tr.nextInt();
+				System.out.print("h=");
+				int h = tr.nextInt();
+				System.out.print("S=" +(a*h)/2);  */
+	}
+}
+
+		
